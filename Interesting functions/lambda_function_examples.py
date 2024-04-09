15e2f960c59c2a1e918a0f142854e04170cc91d0
Yoeri Samwel - DSV <yoeri.samwel@dsv.com>
2024-04-09 08:48:44 -0400

    added lambda function examples

---
+++ Interesting functions/lambda_function_examples.py
@@ -0,0 +1,130 @@
+# Example
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+# Explanation
+#adding two lists together
+combined_list = list(map(lambda x, y: x + y, list1, list2))
+
+print(combined_list)
+
+# The expected output should be: [7, 9, 11, 13, 15]
+
+# Example removing non values
+your_list = [1, 2, 3, 4, 5]
+
+# Explanation
+filtered_list = list(filter(lambda x: x is not None, your_list))
+
+print(filtered_list)
+
+# The expected output should be: [1, 2, 3, 4, 5]
+
+# Example List For filtering a list to select even numbers:
+your_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Explanation
+filtered_list = list(filter(lambda x: x % 2 == 0, your_list))
+
+print(filtered_list)
+#Expected output: [2, 4, 6, 8, 10]
+
+# Example List For mapping a list to square each element:
+your_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Explanation
+squared_list = list(map(lambda x: x ** 2, your_list))
+
+print(squared_list)
+#Expected output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Example List To filter even numbers from a list:
+your_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Explanation
+filtered_list = list(filter(lambda x: x % 2 == 0, your_list))
+
+print(filtered_list)
+#The expected output for this code is: [2, 4, 6, 8, 10]
+
+# Example List To square each element in a list:
+your_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Explanation
+squared_list = list(map(lambda x: x ** 2, your_list))
+
+print(squared_list)
+#The expected output for this code is: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Example For conditional mapping of a list (square if even, otherwise leave the element as is):
+your_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Explanation
+modified_list = list(map(lambda x: x ** 2 if x % 2 == 0 else x, your_list))
+
+print(modified_list)
+#The expected output for this snippet is: [1, 4, 3, 16, 5, 36, 7, 64, 9, 100]
+
+
+# Example For filtering a dictionary based on values (keep only entries where the value is even):
+your_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+# Filters out key-value pairs where the value is even
+filtered_dict = {k: v for k, v in your_dict.items() if v % 2 == 0}
+
+print(filtered_dict)
+#The expected output for this snippet is: {'b': 2, 'd': 4}
+
+# Example Here's the code for sorting a list of dictionaries by a specific key as shown in your image:
+your_list_of_dicts = [
+    {'a': 1},
+    {'b': 2},
+    {'c': 3},
+    {'d': 4},
+    {'e': 5}
+]
+
+# Sorting based on the keys of the dictionaries
+sorted_dict_list = sorted(your_list_of_dicts, key=lambda x: list(x.keys())[0])
+
+print(sorted_dict_list)
+#The expected output is a sorted list of dictionaries by their keys: [{'a': 1}, {'b': 2}, {'c': 3}, {'d': 4}, {'e': 5}]
+
+
+# Example To reverse a string:
+s = "clcoding"
+
+# Explanation
+reversed_string = lambda s: s[::-1]
+
+print(reversed_string(s))
+#When s is "clcoding", the expected output will be "gnidolc".
+
+# Example To extract the first N characters from a string:
+s = "clcoding"
+n = 3
+
+# Explanation
+first_n_chars = lambda s, n: s[:n]
+
+print(first_n_chars(s, n))
+#With s as "clcoding" and n as 3, the expected output will be "clc".
+
+# Example Checking if a string is a palindrome:
+s = "clcoding"
+
+# Explanation
+is_palindrome = lambda s: s == s[::-1]
+
+print(is_palindrome(s))
+#The output for the string "clcoding" will be False because it is not a palindrome.
+
+# Example Counting the number of vowels in a string:
+s = "clcoding"
+
+# Explanation
+count_vowels = lambda s: sum(1 for char in s if char.lower() in 'aeiou')
+
+print(count_vowels(s))
+#For the string "clcoding", the output will be 2 because there are two vowels ('o' and 'i').
+
